dff2648cdb89e0a82ce082a18cbafbe1ef79d964
KunalBhoyar <bhoyar.k@northestern.edu>
2023-02-09 03:37:42 -0500

    Final Commit

--- Schemas.py
+++ Schemas.py
@@ -1,33 +1,29 @@
-from sqlalchemy import String, Column, DateTime, Integer
 import datetime
 from sqlalchemy import ForeignKey
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import String, Column, DateTime, Integer
+from sqlalchemy.orm import DeclarativeBase, declarative_base
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 
-Base = declarative_base()
-
-
-
+class Base(DeclarativeBase):
+    pass
 
 
 class User(Base):
-    __tablename__ = "Users"
+    __tablename__ = "users"
     
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=False)
-    first_name: Mapped[str] = mapped_column(String(40))
-    last_name: Mapped[str] = mapped_column(String(40))
-    password: Mapped[str] = mapped_column(String(150))
-    username: Mapped[str] = mapped_column(String(50), unique=True)
+    first_name: Mapped[str] = mapped_column(String(30))
+    last_name: Mapped[str] = mapped_column(String(30))
+    password: Mapped[str] = mapped_column(String(100))
+    username: Mapped[str] = mapped_column(String(40), unique=True)
     account_created = Column(DateTime, default=datetime.datetime.isoformat(datetime.datetime.now()))
-    account_updated = Column(DateTime, default=datetime.datetime.isoformat(datetime.datetime.now()))
-    access_token: Mapped[str] = mapped_column(String(100))
-
+    account_updated = Column(DateTime, default=datetime.datetime.isoformat(datetime.datetime.now()))    
 
 
 class Product(Base):
 
-    __tablename__ = "Products"
+    __tablename__ = "products"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=False)
     name: Mapped[str] = mapped_column(String(30))
@@ -37,4 +33,5 @@ class Product(Base):
     quantity: Mapped[int] = mapped_column(Integer)
     date_added = Column(DateTime, default=datetime.datetime.isoformat(datetime.datetime.now()))
     date_last_updated = Column(DateTime, default=datetime.datetime.isoformat(datetime.datetime.now()))
-    owner_user_id: Mapped[int] = mapped_column(ForeignKey("users_table1.id"))
+    owner_user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    
